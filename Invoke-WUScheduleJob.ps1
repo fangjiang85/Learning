04d542d9992999d405d35e8633c7b7b382f5d32a
unknown <fangjiang85@yahoo.com>
2022-06-03 21:20:37 -0700

    powershell function

---
+++ Invoke-WUScheduleJob.ps1
@@ -0,0 +1,16 @@
+#This Script is for remotely triggering Windows Update for Server by using the PSWindowsUpdate module
+
+function FunctionName {
+ 
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true,
+    ValueFromPipelineByPropertyName
+    )]
+    [String]
+    $ParameterName
+)
+
+
+}
+
