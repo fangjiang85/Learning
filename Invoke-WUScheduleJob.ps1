cd73eeae9d91ba67b3a78c04051b04dbb6c6056f
fang <fangjiang85@yahoo.com>
2022-06-25 18:57:34 -0700

    6-25-22

--- Invoke-WUScheduleJob.ps1
+++ Invoke-WUScheduleJob.ps1
@@ -1,20 +1,35 @@
 #This Script is for remotely triggering Windows Update for Server by using the PSWindowsUpdate module
 
-function FunctionName {
+Function Invoke-WuScheduleJob {
  
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true,
-    ValueFromPipelineByPropertyName
-    )]
-    [String]
-    $ParameterName
-)
- If (-not (Import-Module PSWindowsUpdate)){
-      
-      Install-Module PSWindowsUpdate -Force
-      
-    }     
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName
+        )]
+        [String[]]
+        $ComputerName
+    )
 
-}
+    Begin {
+         
+        Try {Import-Module PSWindowsUpdate -ErrorAction Stop}
+      
+        Catch  { Install-Module PSWindowsUpdate -Force}           
 
+    }
+
+    Process {
+
+        Foreach($Computer in $ComputerName){
+
+            Try {Import-Module PSWindowsUpdate -ErrorAction Stop}
+      
+            Catch  { Install-Module PSWindowsUpdate -Force}   
+
+            Invoke-WUJob -ComputerName $Computer -Script {Install-WindowsUpdate -Install -AcceptAll -AutoReboot -PSWUSettings -}
+        }
+
+        }
+
+    }
